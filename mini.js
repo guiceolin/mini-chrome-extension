f5ed4bf373109c92c2836363fa766168366dfdb3
Guilherme Ceolin <guiceolin@gmail.com>
2016-08-16 14:08:27 -0300

    Initial commit

---
+++ mini.js
@@ -0,0 +1,12 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var text = tab.url
+  var copyDiv = document.createElement('div');
+  copyDiv.contentEditable = true;
+  document.body.appendChild(copyDiv);
+  copyDiv.innerHTML = text;
+  copyDiv.unselectable = "off";
+  copyDiv.focus();
+  document.execCommand('SelectAll');
+  document.execCommand("Copy", false, null);
+  document.body.removeChild(copyDiv);
+});
