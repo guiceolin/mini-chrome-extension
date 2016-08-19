c3d2c4d8430a7e90a5ede5c020321346efe2bbef
Guilherme Ceolin <guiceolin@gmail.com>
2016-08-18 22:52:27 -0300

    change icon back to favicon after copy

--- mini.js
+++ mini.js
@@ -9,21 +9,27 @@ function copyToClipboard(text) {
   document.execCommand("Copy", false, null);
   document.body.removeChild(copyDiv);
 }
+
+function setIcon(tab, action, icon) {
+  action.setIcon({
+    path: icon + ".png",
+    tabId: tab.id
+  });
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   var original = tab.url
 
   fetch("http://m.ceol.in/urls.json", {
     method: 'post',
     headers: {
-          "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-        },
+      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+    },
     body: 'original=' + original
   }).then(function(res) {return res.json()} ).then(function(response){
     copyToClipboard(response.url.short)
-    chrome.browserAction.setIcon({
-      path: "done.png",
-      tabId: tab.id
-    });
+    setIcon(tab, chrome.browserAction, "done")
+    setTimeout(function() { setIcon(tab, chrome.browserAction, 'favicon') }, 1000);
   }).catch(function(){
     console.log('oh noes')
   })
